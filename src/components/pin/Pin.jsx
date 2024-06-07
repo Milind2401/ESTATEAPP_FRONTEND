1e503e14e61f93385a876815d31120638e6b1d5c
Milind241 <milinddadore123@gmail.com>
2024-06-07 15:12:46 +0530

    added mark to map

--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -3,15 +3,24 @@ import "./pin.scss";
 import { Link } from "react-router-dom";
 
 function Pin({ item }) {
+  const icon = new L.Icon({
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
   return (
-    <Marker position={[item.latitude, item.longitude]}>
+    <Marker position={[item.latitude, item.longitude]} icon={icon}>
       <Popup>
         <div className="popupContainer">
           <img src={item.img} alt="" />
           <div className="textContainer">
             <Link to={`/${item.id}`}>{item.title}</Link>
             <span>{item.bedroom} bedroom</span>
-            <b>$ {item.price}</b>
+            <b>Rs. {item.price}</b>
           </div>
         </div>
       </Popup>
