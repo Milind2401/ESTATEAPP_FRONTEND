80f8f56c58e032d14135ba639232430e311f5cfa
Milind241 <milinddadore123@gmail.com>
2024-06-07 15:17:16 +0530

    .

--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -1,19 +1,16 @@
 import { Marker, Popup } from "react-leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
+import marker from '../../Assets/icons/Location.svg';
+import { Icon } from 'leaflet'
 
 function Pin({ item }) {
-  const icon = new L.Icon({
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-  });
+const myIcon = new Icon({
+ iconUrl: marker,
+ iconSize: [32,32]
+})
   return (
-    <Marker position={[item.latitude, item.longitude]} icon={icon}>
+    <Marker position={[item.latitude, item.longitude]} icon={myIcon}>
       <Popup>
         <div className="popupContainer">
           <img src={item.img} alt="" />
